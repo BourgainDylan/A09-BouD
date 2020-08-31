a38f317ad528f29a40741cb843ec5a116f28e52d
Dylan Bourgain <dylan.bourgain@gmail.com>
2020-08-31 11:31:38 +0200

    first commit

---
+++ functions.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace boud;
+
+function checkPassword($password){
+
+    
+echo'yo';
+
+
+        // Presence de UPPERCASE
+
+        if(preg_match ('@[A-Z]@', $password)){
+
+
+         if(preg_match ('@[a-z]@', $password)){
+
+            
+         }
+
+        }
+
+        // Presence de LOWERCASE
+
+
+     
+     // Presence NUMBER
+
+        if(preg_match ('@[09]@', $password)){
+
+            
+        }
+
+
+
+    /**
+     * check if the password contains at least:
+     *     - 1 number
+     *     - 1 lowercase
+     *     - 1 uppercase
+     *     - 1 special character
+     *     - 12 characters
+     * 
+     * progress-bar for password strength
+     * with 5 different bootstrap colors
+     * 
+     * display password in <h2>
+     * display message in <h3>
+     */
+
+}
