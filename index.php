ee926d3e83581f4155aac49ba17f361803407435
Dylan Bourgain <dylan.bourgain@gmail.com>
2020-09-02 10:16:14 +0200

    completed

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
         //   if(isset ($_GET['value'])){
             // $password =$_GET['value'];
 
-            $password ="ab@*11111aaaBcaa";
+            $password ="";
             boud\checkPassword($password);
 
 
